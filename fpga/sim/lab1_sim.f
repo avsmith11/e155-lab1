36b715b35e098bdc0bc54fa1b3e47e83297785ff
avsmith11 <avsmith@hmc.edu>
2024-09-03 01:10:45 -0700

    complete and reorganized

---
+++ fpga/sim/lab1_sim.f
@@ -0,0 +1,24 @@
+-L work
+-reflib pmi_work
+-reflib ovi_ice40up
+
+
+"C:/Users/oaksh/Desktop/E155/lab1/fpga/src/lab1_as.sv" 
+-sv
+-optionset VOPTDEBUG
++noacc+pmi_work.*
++noacc+ovi_ice40up.*
+
+-vopt.options
+  -suppress vopt-7033
+-end
+
+-gui
+-top lab1_as
+-vsim.options
+  -suppress vsim-7033,vsim-8630,3009,3389
+-end
+
+-do "view wave"
+-do "add wave /*"
+-do "run 100 ns"
--- fpga/source/lab1_as.sv
+++ fpga/source/lab1_as.sv
@@ -1,24 +1,24 @@
 // Avery Smith - avsmith@hmc.edu, 9/2/24
 // driver for seven-segment led display
 module lab1_as (
-	input logic clk,
 	input logic [3:0] s,
 	output logic [2:0] led,
 	output logic [6:0] seg
 );
 	
 	//Internal high-speed oscillator (24 MHz)
+	logic int_osc;
 	HSOSC #(.CLKHF_DIV(2'b01)) 
          hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
 	
 	// instantiate segment driver
-	segment_driver sd(clk, s, seg);
+	segment_driver sd(s, seg);
 	
 	// non-segmented led logic
-	logic int_osc;
 	logic [24:0] counter = 0;
 	assign led[0] = s[1] ^ s[0];
 	assign led[1] = s[3] & s[2];
+	
 	//blink led[2] every 10,000,000 HSOSC cycles
 	always_ff @(posedge int_osc) begin
 		counter <= counter + 1;
@@ -31,24 +31,24 @@ module lab1_as (
 endmodule
 
 module segment_driver (
-	input logic clk,
 	input logic [3:0] s,
 	output logic [6:0] seg
 );
 
 	always_comb begin
 		case (s)
-			4'h0: seg = ~(7'b1111110);
-			4'h1: seg = ~(7'b0110000);
-			4'h2: seg = ~(7'b1101101);
-			4'h3: seg = ~(7'b1111001);
-			4'h4: seg = ~(7'b0110011);
-			4'h5: seg = ~(7'b1011011);
-			4'h6: seg = ~(7'b1011111);
-			4'h7: seg = ~(7'b1110000);
+			// segments g:a
+			4'h0: seg = ~(7'b0111111);
+			4'h1: seg = ~(7'b0000110);
+			4'h2: seg = ~(7'b1011011);
+			4'h3: seg = ~(7'b1001111);
+			4'h4: seg = ~(7'b1100110);
+			4'h5: seg = ~(7'b1101101);
+			4'h6: seg = ~(7'b1111101);
+			4'h7: seg = ~(7'b0000111);
 			4'h8: seg = ~(7'b1111111);
-			4'h9: seg = ~(7'b1111011);
-			// include letters for later
+			4'h9: seg = ~(7'b1101111);
+			// include letters later
 			default: seg = 7'b1111111;
 		endcase
 	end
